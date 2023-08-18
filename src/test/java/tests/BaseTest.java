43254502584e8fa8cbef109f75d5e8efec7c8504
Stovbchatyi <maksym.stovbchatyi@avenga.com>
2023-08-18 15:35:48 +0300

    none

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -20,7 +20,7 @@ public abstract class BaseTest {
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.driverManagerEnabled = true;
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.browserSize = "1920x1080";
     }
 
